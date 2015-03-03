a1b458da1e01772dfcb1fcd3aff97244c2f59154
tahinim <tahinim@hotmail.com>
2015-03-03 05:33:24 +0530

    html and css

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var $btnShow = $('.btn-show-hide');
+var $box = $('.box');
+
+$btnShow.on ('click', function() {
+    $box.toggleClass('js-box-show-hide');
+});
+
+var $btnmove = $('.btn-move');
+var $pink = $('.diamond');
+
+$btnmove.on ('click',function () {
+    $pink.toggleClass ('js-diamond-slide');
+});
+
+
+
+
+
+
+
+
+
+var $btnExpand = $('.btn-collapse-expand');
+var $panel = $('.panel');
+
+$btnExpand.on ('click', function () {
+    $panel.toggleClass('js-panel-collapse-expand');
+});
+
+
